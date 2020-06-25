c51979c855ab2bf42e1ceef8d10a3313b3e2eb16
Oliver Jose Fermin Valdez <oliver_fermin@ucne.edu.do>
2020-06-24 22:57:40 -0400

    Añadiendo migracion y base de datos

---
+++ OrderDetailApp/Migrations/20200625025443_Migracion inicial.cs
@@ -0,0 +1,126 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace OrderDetailApp.Migrations
+{
+    public partial class Migracioninicial : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Ordenes",
+                columns: table => new
+                {
+                    OrderId = table.Column<int>(nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    Fecha = table.Column<DateTime>(nullable: false),
+                    SuplidorId = table.Column<int>(nullable: false),
+                    Monto = table.Column<double>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Ordenes", x => x.OrderId);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Productos",
+                columns: table => new
+                {
+                    ProductoId = table.Column<int>(nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    Descripcion = table.Column<string>(nullable: false),
+                    Costo = table.Column<double>(nullable: false),
+                    Inventario = table.Column<double>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Productos", x => x.ProductoId);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Suplidores",
+                columns: table => new
+                {
+                    SuplidorId = table.Column<int>(nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    Nombre = table.Column<string>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Suplidores", x => x.SuplidorId);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "OrdenesDetalle",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    ProductoId = table.Column<int>(nullable: false),
+                    OrderId = table.Column<int>(nullable: false),
+                    Cantidad = table.Column<double>(nullable: false),
+                    Costo = table.Column<double>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_OrdenesDetalle", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_OrdenesDetalle_Ordenes_OrderId",
+                        column: x => x.OrderId,
+                        principalTable: "Ordenes",
+                        principalColumn: "OrderId",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.InsertData(
+                table: "Productos",
+                columns: new[] { "ProductoId", "Costo", "Descripcion", "Inventario" },
+                values: new object[] { 1, 100.0, "Arroz", 30.0 });
+
+            migrationBuilder.InsertData(
+                table: "Productos",
+                columns: new[] { "ProductoId", "Costo", "Descripcion", "Inventario" },
+                values: new object[] { 2, 150.0, "Aceite", 15.0 });
+
+            migrationBuilder.InsertData(
+                table: "Productos",
+                columns: new[] { "ProductoId", "Costo", "Descripcion", "Inventario" },
+                values: new object[] { 3, 50.0, "Habichuelas", 40.0 });
+
+            migrationBuilder.InsertData(
+                table: "Suplidores",
+                columns: new[] { "SuplidorId", "Nombre" },
+                values: new object[] { 1, "Surtidora Oliver" });
+
+            migrationBuilder.InsertData(
+                table: "Suplidores",
+                columns: new[] { "SuplidorId", "Nombre" },
+                values: new object[] { 2, "Almacen Fermin" });
+
+            migrationBuilder.InsertData(
+                table: "Suplidores",
+                columns: new[] { "SuplidorId", "Nombre" },
+                values: new object[] { 3, "Suplidora batista" });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_OrdenesDetalle_OrderId",
+                table: "OrdenesDetalle",
+                column: "OrderId");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "OrdenesDetalle");
+
+            migrationBuilder.DropTable(
+                name: "Productos");
+
+            migrationBuilder.DropTable(
+                name: "Suplidores");
+
+            migrationBuilder.DropTable(
+                name: "Ordenes");
+        }
+    }
+}
